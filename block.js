45f8b122d07b44e9903e182d96d2a021ff81e249
dreak603 <dreak603@gmail.com>
2019-08-21 16:39:46 +0800

    add blockchain and unit test

--- block.js
+++ block.js
@@ -1,38 +1,43 @@
 const SHA256 = require('crypto-js/sha256')
 
 class Block {
-    constructor(timestamp, lastHash, hash, data) {
-        this.timestamp = timestamp
-        this.lastHash = lastHash
-        this.hash = hash
-        this.data = data
-    }
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp
+    this.lastHash = lastHash
+    this.hash = hash
+    this.data = data
+  }
 
-    toString() {
-        return `Block -
+  toString() {
+    return `Block -
         Timestamp: ${this.timestamp}
         Last Hash: ${this.lastHash.substring(0, 10)}
         hash     : ${this.hash.substring(0, 10)}
         Data     " ${this.data}
       `
-    }
+  }
 
-    /** identifier enable us to enter directly without having new instance, you can directly use etc. Block.gensis() in other file  */
-    static genesis() {
-        return new this('Genesis time', '-----', 'f1r57-h45h', []) // this of the block itself
-    }
+  /** identifier enable us to enter directly without having new instance, you can directly use etc. Block.gensis() in other file  */
+  static genesis() {
+    return new this('Genesis time', '-----', 'f1r57-h45h', []) // this of the block itself
+  }
 
-    static mineBlock(lastBlock, data) {
-        const timestamp = Date.now()
-        const lastHash = lastBlock.hash
-        const hash = Block.hash(timestamp, lastHash, data)
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now()
+    const lastHash = lastBlock.hash
+    const hash = Block.hash(timestamp, lastHash, data)
 
-        return new this(timestamp, lastHash, hash, data)
-    }
+    return new this(timestamp, lastHash, hash, data)
+  }
 
-    static hash(timestamp, lastHash, data) {
-        return SHA256(`${timestamp}${lastHash}${data}`).toString()
-    }
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString()
+  }
+
+  static blockHash(block) {
+    const { timestamp, lastHash, data } = block
+    return Block.hash(timestamp, lastHash, data)
+  }
 }
 
 module.exports = Block
